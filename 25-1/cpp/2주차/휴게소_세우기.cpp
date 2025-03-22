25d7494527206ca5850e7e28fbb7489e5a2a08c4
kss418 <108602943+kss418@users.noreply.github.com>
2025-03-22 13:24:07 +0900

    Update 휴게소_세우기.cpp

--- 25-1/cpp/2주차/휴게소_세우기.cpp
+++ 25-1/cpp/2주차/휴게소_세우기.cpp
@@ -5,13 +5,17 @@ using namespace std;
 int a[1010], n, m, l;
 
 bool decision(int cur){
+    // 휴게소 사이의 거리의 최댓값을 cur로 만들기 위해
+    // 필요한 추가 휴게소의 개수
     int cnt = 0;
     for(int i = 1;i <= n + 1;i++){
-        int diff = a[i] - a[i - 1];
-        cnt += diff / cur;
-        if(diff % cur == 0) cnt--;
+        int diff = a[i] - a[i - 1]; // 휴게소의 거리
+        cnt += diff / cur; // (차이 / 최댓값) 만큼 추가로 휴게소를 설치
+        if(diff % cur == 0) cnt--; // 차이가 최댓값의 배수면 1을 빼줌
     }
 
+    // 설치해야 하는 휴게소의 개수가 M 이하면 1
+    // 아니면 0
     return cnt <= m;
 }
 
@@ -35,11 +39,10 @@ int minimazation(){
 int main(){
     cin >> n >> m >> l;
     for(int i = 1;i <= n;i++) cin >> a[i];
-    a[0] = 0; a[n + 1] = l;
-    sort(a, a + n + 2);
+    a[0] = 0; a[n + 1] = l; // 시작점 0, 끝점 l
+    sort(a, a + n + 2); // 정렬
 
     cout << minimazation();
-
     return 0;
 }
 
