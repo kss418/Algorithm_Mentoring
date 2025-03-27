374d5aceac88f9d9f4d90951bd1fd10c32e1abe3
kss418 <jis1595@naver.com>
2025-03-27 23:35:27 +0900

    공유기 설치 소스 코드 추가

--- 25-1/cpp/2주차/공유기_설치.cpp
+++ 25-1/cpp/2주차/공유기_설치.cpp
@@ -7,8 +7,8 @@ const ll MAX = 201010;
 ll a[MAX], n, m;
 
 bool decision(ll cur){
-    // cnt > 설치한 공유기 개수
-    // last > 마지막으로 설치한 공유기 위치
+    // cnt -> 설치한 공유기 개수
+    // last -> 마지막으로 설치한 공유기 위치
     ll cnt = 0, last = -1e12; // last는 음의 무한대로 초기화
     for(int i = 1;i <= n;i++){
         // 마지막으로 설치한 공유기의 위치와
