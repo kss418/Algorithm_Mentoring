08526f2b08540094c3b50072a59df22e5fefc5d5
kss418 <jis1595@naver.com>
2025-04-04 15:05:41 +0900

    가운데를 말해요 주석 추가

--- 25-1/cpp/3주차/가운데를_말해요.cpp
+++ 25-1/cpp/3주차/가운데를_말해요.cpp
@@ -15,16 +15,21 @@ int main(){
     cin >> n;
     for(int i = 1;i <= n;i++){
         ll x; cin >> x;
+        // 홀수 번째에는 최대 우선순위 큐에 삽입
         if(i % 2) pq1.push(x);
+        // 짝수 번째에는 최소 우선순위 큐에 삽입
         else pq2.push(x);
 
+        // 최대 우선순위 큐의 값이 최소 우선순위 큐의 값보다 크면
         if(!pq2.empty() && pq1.top() > pq2.top()){
             ll t1 = pq1.top();
             ll t2 = pq2.top();
+            // 서로 스왑해 줌
             pq1.pop(); pq2.pop();
             pq1.push(t2); pq2.push(t1);
         }
 
+        // 최대 우선순위 큐의 최댓값이 중간 값
         cout << pq1.top() << "\n";
     }
 }
