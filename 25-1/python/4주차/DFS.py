45103072b63997d1c109729f919c481c16a9389f
kss418 <jis1595@naver.com>
2025-04-04 19:13:44 +0900

    DFS 코드 추가

---
+++ 25-1/python/4주차/DFS.py
@@ -0,0 +1,28 @@
+import sys
+input = sys.stdin.readline
+v = [0] * 10
+a = [[]] * 10
+
+def add(s, e):
+    # 양방향 간선 추가
+    a[s].append(e)
+    a[e].append(s)
+
+def dfs(cur):
+    # 방문한 정점으로 표시
+    v[cur] = 1
+    print(cur)
+
+    for nxt in a[cur]:
+        # 다음 정점이 방문한 정점이면 넘어 감
+        if v[nxt] == 1:
+            continue
+        dfs(nxt)
+
+add(1, 2)
+add(1, 3)
+add(1, 4)
+add(3, 4)
+add(4, 5)
+
+dfs(1)
