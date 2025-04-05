6ce442670ce9559e2fc7f8a59faf99f897c75f1b
kss418 <jis1595@naver.com>
2025-04-05 09:10:45 +0900

    토마토 코드 추가

---
+++ 25-1/cpp/4주차/토마토.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+using namespace std;
+using ll = long long;
+
+const ll MAX = 1010;
+const ll INF = 1e9;
+ll dx[4] = {0, 0, 1, -1}, dy[4] = {1, -1, 0, 0};
+ll d[MAX][MAX], a[MAX][MAX], n, m;
+
+class node{
+public:
+    ll y, x;
+};
+queue <node> q;
+
+bool outrange(ll cy, ll cx){
+    if(cy <= 0 || cx <= 0 || cy > n || cx > m) return 1;
+    return a[cy][cx] == -1;
+}
+
+void bfs(){
+    while(!q.empty()){
+        auto [cy, cx] = q.front(); q.pop();
+        for(int i = 0;i < 4;i++){
+            ll ny = cy + dy[i], nx = cx + dx[i];
+            if(outrange(ny, nx)) continue;
+            if(d[ny][nx] != INF) continue;
+            d[ny][nx] = d[cy][cx] + 1;
+            q.push({ny, nx});
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(0); // fastio
+    cin.tie(0), cout.tie(0); // fastio
+
+    cin >> m >> n;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= m;j++){
+            cin >> a[i][j];
+            d[i][j] = INF;
+
+            if(a[i][j] == 1){
+                d[i][j] = 0;
+                q.push({i, j});
+            }
+        }
+    }
+
+    bfs();
+    ll result = 0;
+    for(int i = 1;i <= n;i++){
+        for(int j = 1;j <= m;j++){
+            if(a[i][j] == -1) continue;
+            result = max(result, d[i][j]);
+        }
+    }
+
+    cout << (result == INF ? -1 : result);
+
+    return 0;
+}
