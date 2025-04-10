d30aca299902a3a84dac6e3c61eb79ce3fea1d82
kss418 <jis1595@naver.com>
2025-04-10 17:43:19 +0900

    이분 그래프 주석 추가

--- 25-1/cpp/4주차/이분_그래프.cpp
+++ 25-1/cpp/4주차/이분_그래프.cpp
@@ -9,13 +9,18 @@ vector <ll> a[MAX];
 ll v[MAX], result, n, m;
 
 void dfs(ll cur, ll num){
+    // 현재 정점의 번호 설정
     v[cur] = num;
     
     for(auto& nxt : a[cur]){
+        // 다음 정점을 방문 했으면
         if(v[nxt] != -1){
+            // 배치할 번호와 이미 배치 된 번호가 다르면 NO
             if(v[nxt] != num ^ 1) result = 0;
             continue;
         }
+
+        // 번호를 바꿔서 탐색
         dfs(nxt, num ^ 1); 
     }
 }
@@ -31,18 +36,22 @@ void solve(){
 
     result = 1;
     for(int i = 1;i <= n;i++) {
+        // 간선 리스트와 방문 배열 초기화
         a[i].clear(); v[i] = -1;
     }
 
     while(m--){
+        // 간선 추가
         ll s, e; cin >> s >> e;
         add(s, e);
     }
 
     for(int i = 1;i <= n;i++){
+        // 방문을 했으면 넘어 감
         if(v[i] != -1) continue;
         dfs(i, 0);
     }
+
     cout << (result ? "YES" : "NO") << "\n";
 }
 
--- 25-1/python/4주차/이분_그래프.py
+++ 25-1/python/4주차/이분_그래프.py
@@ -12,27 +12,34 @@ def add(s, e):
 
 def dfs(cur, num):
     global result
+    # 현재 정점의 번호 설정
     v[cur] = num
 
     for nxt in a[cur]:
+        # 다음 정점을 방문 했으면
         if v[nxt] != -1:
-            if(v[nxt] != num ^ 1):
+            # 배치할 번호와 이미 배치 된 번호가 다르면 NO
+            if v[nxt] != num ^ 1:
                 result = 0
             continue
+        # 번호를 바꿔서 탐색
         dfs(nxt, num ^ 1)
             
 t = int(input().rstrip())
 for _ in range(t):
+    # 방문 배열과 간선 리스트 초기화
     v = [-1] * 20101
     a = [[] for _ in range(20101)]
     result = 1
 
     n, m = list(map(int, input().rstrip().split()))
+    # 간선 추가
     for _ in range(m):
         s, e = list(map(int, input().rstrip().split()))
         add(s, e)
     
     for i in range(1, n + 1):
+        # 방문을 했으면 넘어 감
         if v[i] != -1:
             continue
         dfs(i, 0)
