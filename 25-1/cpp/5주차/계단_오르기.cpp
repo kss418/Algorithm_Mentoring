64adb99d96cac769dc98799692cd876376a0c998
kss418 <jis1595@naver.com>
2025-04-12 15:31:46 +0900

    계단 오르기 주석 추가

--- 25-1/cpp/5주차/계단_오르기.cpp
+++ 25-1/cpp/5주차/계단_오르기.cpp
@@ -12,14 +12,21 @@ int main(){
 
     cin >> n;
     for(int i = 1;i <= n;i++) cin >> a[i];
+
+    // Base Case
+    dp[1][0] = a[1];
+    dp[2][0] = a[2];
+    dp[2][1] = a[1] + a[2];
     
-    for(int i = 1;i <= n;i++){
+    for(int i = 3;i <= n;i++){
+        // 전의 계단을 밟았으면 전의 계단에서 점화식 전이
 		dp[i][1] = dp[i - 1][0] + a[i];
 
-        if(i >= 2) dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + a[i];
-        else dp[i][0] = a[i];
+        // 전의 계단을 밟지 않았으면 2칸 전의 계단에서 점화식 전이
+        dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + a[i];
     }
 
+    // 둘 중 큰 값이 정답
     cout << max(dp[n][0], dp[n][1]);
 
     return 0;
--- 25-1/python/5주차/계단_오르기.py
+++ 25-1/python/5주차/계단_오르기.py
@@ -10,12 +10,19 @@ for _ in range(n):
 
 dp = [[0, 0] for _ in range(303)]
 
-for i in range(1, n + 1):
-    if(i >= 2):
-        dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + a[i]
-    else:
-        dp[i][0] = a[i]
+# Base Case
+dp[1][0] = a[1]
+if(n >= 2):
+    dp[2][0] = a[2]
+    dp[2][1] = a[1] + a[2]
 
+for i in range(3, n + 1):
+    # 전의 계단을 밟았으면 전의 계단에서 점화식 전이
     dp[i][1] = dp[i - 1][0] + a[i]
 
+    # 전의 계단을 밟지 않았으면 2칸 전의 계단에서 점화식 전이
+    dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + a[i]
+
+# 둘 중 큰 값이 정답
 print(max(dp[n][0], dp[n][1]))
+
