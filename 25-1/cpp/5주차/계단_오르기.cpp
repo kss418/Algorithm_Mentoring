1d6ca052b996daefaad022c2b78bf81eeceadf5c
kss418 <jis1595@naver.com>
2025-04-12 15:01:49 +0900

    피보나치 수열 추가

--- 25-1/cpp/5주차/계단_오르기.cpp
+++ 25-1/cpp/5주차/계단_오르기.cpp
@@ -15,6 +15,7 @@ int main(){
     
     for(int i = 1;i <= n;i++){
 		dp[i][1] = dp[i - 1][0] + a[i];
+
         if(i >= 2) dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + a[i];
         else dp[i][0] = a[i];
     }
