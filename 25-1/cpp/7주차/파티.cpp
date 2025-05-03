94986fef219b392b13f0b5fb522cfe08e8142202
kss418 <jis1595@naver.com>
2025-05-03 14:19:40 +0900

    파티 코드 수정

--- 25-1/cpp/7주차/파티.cpp
+++ 25-1/cpp/7주차/파티.cpp
@@ -6,8 +6,8 @@ using ll = long long;
 
 const ll MAX = 1010;
 const ll INF = 1e12;
-ll n, m, x, d[MAX][MAX];
-vector <pair<ll, ll>> adj[MAX];
+ll n, m, x, d[MAX][2];
+vector <pair<ll, ll>> adj[MAX][2];
 
 using pll = pair<ll, ll>;
 priority_queue <pll, vector<pll>, greater<pll>> pq;
@@ -19,24 +19,25 @@ int main(){
     cin >> n >> m >> x;
     while(m--){
         ll s, e, c; cin >> s >> e >> c;
-        adj[s].push_back({e, c});
+        adj[s][0].push_back({e, c});
+        adj[e][1].push_back({s, c});
     }
 
     for(int i = 0;i < MAX;i++){
-        for(int j = 0;j < MAX;j++) d[i][j] = INF;
+        for(int j = 0;j < 2;j++) d[i][j] = INF;
     }
 
-    for(int i = 1;i <= n;i++){
+    for(int i = 0;i <= 1;i++){
         while(!pq.empty()) pq.pop();
-        pq.push({0, i});
+        pq.push({0, x});
 
         while(!pq.empty()){
             auto[cd, cur] = pq.top(); pq.pop();
-            if(d[i][cur] <= cd) continue;
-            d[i][cur] = cd;
+            if(d[cur][i] <= cd) continue;
+            d[cur][i] = cd;
 
-            for(auto& [nxt, co] : adj[cur]){
-                if(d[i][nxt] <= cd + co) continue;
+            for(auto& [nxt, co] : adj[cur][i]){
+                if(d[nxt][i] <= cd + co) continue;
                 pq.push({cd + co, nxt});
             }
         }
@@ -44,7 +45,7 @@ int main(){
 
     ll result = 0;
     for(int i = 1;i <= n;i++){
-        result = max(result, d[i][x] + d[x][i]);
+        result = max(result, d[i][0] + d[i][1]);
     }
     cout << result;
 
--- 25-1/python/7주차/파티.py
+++ 25-1/python/7주차/파티.py
@@ -4,31 +4,30 @@ input = sys.stdin.readline
 
 INF = 10**12
 n, m, x = map(int, input().split())
-adj = [[] for _ in range(n+1)]
 
+adj = [[[] for _ in range(2)] for _ in range(n+1)]
 for _ in range(m):
     s, e, c = map(int, input().split())
-    adj[s].append((e, c))
+    adj[s][0].append((e, c))
+    adj[e][1].append((s, c))
 
-d = [[INF] * (n+1) for _ in range(n+1)]
-
-for i in range(1, n+1):
+d = [[INF, INF] for _ in range(n+1)]
+for i in range(2):
     pq = []
-    heapq.heappush(pq, (0, i))  
+    heapq.heappush(pq, (0, x))
     while pq:
         cd, cur = heapq.heappop(pq)
-        if d[i][cur] <= cd:
+        if d[cur][i] <= cd:
             continue
-        d[i][cur] = cd
-        for nxt, co in adj[cur]:
+        d[cur][i] = cd
+        for nxt, co in adj[cur][i]:
             nd = cd + co
-            if d[i][nxt] > nd:
+            if d[nxt][i] > nd:
                 heapq.heappush(pq, (nd, nxt))
 
 result = 0
 for i in range(1, n+1):
-    result = max(result, d[i][x] + d[x][i])
+    result = max(result, d[i][0] + d[i][1])
 
 print(result)
-
 
