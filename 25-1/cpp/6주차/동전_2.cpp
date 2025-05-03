8b4aa0a600e15a2cdeb8b17f0d4eb5a8fc822cc1
kss418 <jis1595@naver.com>
2025-05-03 13:09:26 +0900

    동전 2 코드 추가

--- 25-1/cpp/6주차/동전_2.cpp
+++ 25-1/cpp/6주차/동전_2.cpp
@@ -9,8 +9,14 @@ ll dp[MAX], n, m, a[101];
 ll solve(ll cur){
     if(cur < 0) return INF;
     ll& ret = dp[cur];
-    if(ret != -1) return ret; ret = INF;
+    if(ret != -1) return ret; 
+    // min값을 구하기 위해서 큰 수로 초기화
+    ret = INF;
+
+    // Base Case 
     if(!cur) return ret = 0;
+
+    // 각 동전을 1개 씩 사용해서 cur을 만들 때 최솟값
     for(int i = 1;i <= n;i++){
         ret = min(ret, solve(cur - a[i]) + 1);
     }
@@ -25,6 +31,8 @@ int main(){
     cin >> n >> m;
     for(int i = 1;i <= n;i++) cin >> a[i];
     for(int i = 0;i < MAX;i++) dp[i] = -1;
+
+    // 값이 INF면 M원을 만들 수 없음
     cout << (solve(m) == INF ? -1 : solve(m));
 
     return 0;
--- 25-1/python/6주차/동전_2.py
+++ 25-1/python/6주차/동전_2.py
@@ -9,11 +9,16 @@ def solve(cur):
     
     if dp[cur] != -1:
         return dp[cur]
+    
+    # Base case
     if cur == 0:
         dp[cur] = 0
         return 0
 
+    # min값을 구하기 위해서 큰 수로 초기화
     dp[cur] = INF
+
+    # 각 동전을 1개 씩 사용해서 cur을 만들 때 최솟값
     for i in range(1, n+1):
         dp[cur] = min(dp[cur], solve(cur - a[i]) + 1)
 
@@ -27,4 +32,5 @@ for i in range(1, n+1):
 dp = [-1] * (m+1)
 
 result = solve(m)
+# 값이 INF면 M원을 만들 수 없음
 print(-1 if result == INF else result)
